7180484a1a36a972214a46d9a3f3c74b1bc5b24d
JT <Hawxy@users.noreply.github.com>
2026-08-18 15:21:58 +0800

    Support trusted publishing

---
+++ build/Build.cs
@@ -0,0 +1,102 @@
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[GitHubActions(
+    "Build & Test",
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = ["master"],
+    OnPullRequestBranches = ["master"],
+    InvokedTargets = [nameof(Test)])]
+[TrustedPublishingGitHubActions(
+    "Manual Nuget Push",
+    GitHubActionsImage.UbuntuLatest,
+    On = [GitHubActionsTrigger.WorkflowDispatch],
+    InvokedTargets = [nameof(NugetPush)],
+    NugetUser = "${{ secrets.NUGET_USER }}")]
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            ArtifactsDirectory.CreateOrCleanDirectory();
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration("Release")
+                .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            // Release, matching Compile: reuses its output instead of a second Debug build, and
+            // tests the configuration that ships.
+            DotNetTest(s => s
+                .AddProcessAdditionalArguments("--project", Solution)
+                .AddProcessAdditionalArguments("--configuration", "Release"));
+        });
+
+    static readonly string[] PackableProjects =
+    [
+        "Schematic.DependencyInjection",
+        "Schematic.AspNetCore",
+        "Schematic.Extensions.AI",
+        "Schematic.Extensions.Quartz",
+    ];
+
+    Target NugetPack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            foreach (var name in PackableProjects)
+            {
+                var project = Solution.AllProjects.Single(x => x.Name == name);
+                DotNetPack(_ => _
+                    .SetProject(project)
+                    .SetConfiguration("Release")
+                    .EnableContinuousIntegrationBuild()
+                    .SetOutputDirectory(ArtifactsDirectory));
+            }
+        });
+
+    [Parameter("NuGet API key, short-lived key issued by NuGet/login via trusted publishing")] [Secret] readonly string NugetApiKey;
+
+    Target NugetPush => _ => _
+        .DependsOn(NugetPack)
+        .Requires(() => !string.IsNullOrEmpty(NugetApiKey))
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .EnableSkipDuplicate()
+                .EnableNoSymbols()
+                .SetApiKey(NugetApiKey));
+        });
+}
